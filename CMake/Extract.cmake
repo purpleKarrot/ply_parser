48e8621ea3f5ca39fe4bb0aea4958bd9f41ceec0
Daniel Pfeifer <danielp@vmml-desktop.(none)>
2010-07-19 14:10:26 +0200

    use tar for externals

--- CMake/Extract.cmake
+++ CMake/Extract.cmake
@@ -1,20 +1,6 @@
 
-find_program(7Z_EXECUTABLE 7z $ENV{ProgramFiles}/7-Zip)
-find_program(UNZIP_EXECUTABLE unzip)
-
 function(EXTRACT ARCHIVE DIRECTORY)
   file(MAKE_DIRECTORY ${DIRECTORY})
-
-  if(7Z_EXECUTABLE)
-    execute_process(COMMAND ${7Z_EXECUTABLE} x -y ${ARCHIVE}
-      WORKING_DIRECTORY ${DIRECTORY} OUTPUT_QUIET)
-
-  elseif(UNZIP_EXECUTABLE)
-    execute_process(COMMAND ${UNZIP_EXECUTABLE} -o ${ARCHIVE}
-      WORKING_DIRECTORY ${DIRECTORY} OUTPUT_QUIET)
-
-  else()
-    message(FATAL_ERROR "cannot extract ${ARCHIVE}")
-
-  endif()
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${ARCHIVE}
+    WORKING_DIRECTORY ${DIRECTORY})
 endfunction(EXTRACT ARCHIVE DIRECTORY)
